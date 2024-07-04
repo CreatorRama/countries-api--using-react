3be2f529cf956b168b3db67f9648d24466d6e9e4
Creator Rama <ap410485@gmail.com>
2024-07-04 21:41:16 +0530

    u i.jsx

--- index.jsx
+++ index.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './app';
 import Contact from './components/contact';
 import Home from './components/home';
-import Error from './components/error';
+import ErrorPage from './components/error'; // Renamed Error component to ErrorPage to avoid conflict with built-in Error object
 import Countrydetails from './components/Countrydetails';
 import leftarrow from './assets/left-arrow.png'; // Adjusted path for asset import
 import './country.css';
@@ -11,13 +11,13 @@ import './country.css';
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-  <Router basename='/countries-api--using-react'>
+  <Router basename="/countries-api--using-react">
     <Routes>
-      <Route exact path="/countries-api--using-react" element={<App />}>
+      <Route path="/" element={<App />}>
         <Route index element={<Home />} />
         <Route path="contact" element={<Contact />} />
         <Route path=":country" element={<Countrydetails imageurl={leftarrow} />} />
-        <Route path="*" element={<Error />} />
+        <Route path="*" element={<ErrorPage />} />
       </Route>
     </Routes>
   </Router>
